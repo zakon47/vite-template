2d2397e942975c75b16557bbf4804bb57f329461
3AK <zakon47@gmail.com>
2024-04-10 21:44:23 +0300

    add proxy

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,46 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import s from "./index.module.scss";
 import reactLogo from "@/assets/react.svg";
 import viteLogo from "/vite.svg";
 
+type VersionResponse = {
+  encrypted: boolean;
+  folder: string;
+  swagger: boolean;
+  telegram: boolean;
+  version: string;
+};
+
+interface HomePageState {
+  isLoading: boolean;
+  data: null | VersionResponse;
+}
+
 const HomePage: FC = () => {
   const [count, setCount] = useState(1);
 
-  return (
-    <div className={s.wrap} onClick={() => setCount(count + 1)}>
-      <div className={s.box}>
-        <h2 className={s.h1}>Welcome</h2>
-        {count}
+  const [state, setState] = useState<HomePageState>({
+    isLoading: true,
+    data: null,
+  });
 
-        <div className={s.icons}>
+  useEffect(() => {
+    handleGetData();
+  }, []);
+  const handleGetData = async () => {
+    setState((prevState) => ({ ...prevState, isLoading: true }));
+    const res = await fetch("/api/v1/version");
+    const data = (await res.json()) as unknown as VersionResponse;
+    setState((prevState) => ({ ...prevState, data, isLoading: false }));
+  };
+  return (
+    <div className={s.wrap}>
+      <div className={s.box}>
+        <div className={s.box__top} onClick={() => setCount(count + 1)}>
+          <h2 className={s.h1}>Welcome</h2>
+          {count}
+        </div>
+        <div className={s.box__icons}>
           <a href="https://vitejs.dev" target="_blank">
             <img src={viteLogo} className="logo" alt="Vite logo" />
           </a>
@@ -20,6 +48,30 @@ const HomePage: FC = () => {
             <img src={reactLogo} className="logo react" alt="React logo" />
           </a>
         </div>
+        {state.isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <ul className={s.list}>
+            <li>
+              <u>encrypted:</u> {state.data?.encrypted ? "OK" : "-"}
+            </li>
+            <li>
+              <u>folder:</u> {state.data?.folder}
+            </li>
+            <li>
+              <u>version:</u> {state.data?.version}
+            </li>
+            <li>
+              <u>swagger:</u> {state.data?.swagger ? "OK" : "-"}
+            </li>
+            <li>
+              <u>telegram:</u> {state.data?.telegram ? "OK" : "-"}
+            </li>
+          </ul>
+        )}
+        <button className={s.btn} onClick={handleGetData}>
+          Get Data
+        </button>
       </div>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8200",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+      "/socket.io": {
+        target: "ws://localhost:8200",
+        ws: true,
+      },
+    },
+  },
+});
