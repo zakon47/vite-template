1b472ebc28c62ecc9c7170c6e1cf65d1e9efb5aa
3AK <zakon47@gmail.com>
2024-04-21 01:13:40 +0300

    fix load languages

--- src/i18n/settings.ts
+++ src/i18n/settings.ts
@@ -4,7 +4,7 @@ import HttpBackend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
 import { ENV } from '@/env.ts';
-import { defaultLang, languagesList, ns } from '@/i18n/vars.ts';
+import { languagesList, ns } from '@/i18n/vars.ts';
 
 if (!ENV.I18NEXUS_API_KEY) {
   throw new Error('VITE_I18NEXUS_API_KEY is empty!');
@@ -12,7 +12,7 @@ if (!ENV.I18NEXUS_API_KEY) {
 const loadPath = `https://api.i18nexus.com/project_resources/translations/{{lng}}/{{ns}}.json?api_key=${ENV.I18NEXUS_API_KEY}`;
 
 const config: InitOptions = {
-  fallbackLng: defaultLang,
+  // fallbackLng: defaultLang,  // when to always download
   ns,
   defaultNS: ns[0],
   supportedLngs: languagesList.map((elem) => elem.id),
